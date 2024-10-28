20182da2ae8a58ce280ae9da265d3e7a27acbd73
admin043 <muntashirhassan043@gmail.com>
2024-10-29 00:54:56 +0600

    practice

---
+++ data types , arry , length .py
@@ -0,0 +1,19 @@
+a=10
+type(a)
+
+a=10.5
+type(a)
+
+a='1'
+type=(a)
+
+a=100
+int(a)
+float(a)
+str(a)
+
+fruits='apple'
+fruits[4]
+
+a='dsadfaw    fseffaefes f  aefaefaf '
+len(a)
---
+++ for loop.py
@@ -0,0 +1,15 @@
+a='sjfeba akna afsegegad'
+for x in a:
+print(x)
+
+
+for x in a:
+if x == 'f':
+print('fruit')
+
+
+total=0
+for x in a:
+if x == 'f':
+total=total+1
+print(a)
